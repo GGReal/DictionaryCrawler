015b6473df277bc9a6bf59b0f7720bcb8f6a0096
GGReal <gugangboy@gmail.com>
2015-10-22 20:01:23 +0800

    Update

---
+++ src/com/crawler/model/Character.java
@@ -0,0 +1,13 @@
+package com.crawler.model;
+
+/**
+ * Created by LVY on 2015/10/22.
+ */
+public class Character {
+
+    private string character;
+    private string details;
+    private string website;
+    private string 
+
+}
